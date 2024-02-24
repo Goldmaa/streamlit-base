7004eeeada4a13615e7788f8807a6221a09bd72c
Alex Goldman <90787028+Goldmaa@users.noreply.github.com>
2024-02-24 15:26:13 +0000

    Changed auth mode to streamlit-authenticator

--- README.md
+++
@@ -1,1 +0,0 @@
-Copy, search for st.secrets in streamlit_app.py and set up the necessary secrets - then get going!
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,73 +1,55 @@
+from yaml.loader import SafeLoader
+import yaml
+
 import streamlit as st
-import extra_streamlit_components as stx
-import streamlit_extras as stx2
-from streamlit_login_auth_ui.widgets import __login__
+import streamlit_authenticator as stauth
 
-# Check run mode:
-if st.experimental_user.get("email") == "test@example.com":
-    MODE = "dev"
-else:
-    MODE = "prod"
+# Uncomment below if you need cookies:
+# import extra_streamlit_components as stx
 
-print("Mode:", MODE)
-
-# Define page title:
+# Config page
 PAGE_TITLE = ""
-if PAGE_TITLE == "":
-    raise ValueError("Please define a page title in streamlit_app.py.")
-
+PAGE_ICON = ""
 st.set_page_config(
-    page_title=PAGE_TITLE, page_icon="🔥", layout="wide", initial_sidebar_state="collapsed"
+    PAGE_TITLE, PAGE_ICON, layout="wide", initial_sidebar_state="collapsed"
+)
+#
+
+# Checks for empty variables
+if PAGE_TITLE.isempty():
+    raise ValueError(
+        "Please set the PAGE_TITLE variable to the title of your app")
+
+if PAGE_ICON.isempty():
+    raise ValueError(
+        "Please set the PAGE_ICON variable to the title of your app")
+#
+
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
 )
 
+authenticator.login()
 
-# Check for API key:
-try:
-    st.secrets["courier_api_key"]
-except KeyError:
-    raise ValueError(
-        "No 'courier_api_key' secret found: define this in .streamlit/secrets.toml")
-
-# Check for admin email:
-try:
-    st.secrets["admin_email"]
-except KeyError:
-    raise ValueError(
-        "No 'admin_email' secret found: define this in .streamlit/secrets.toml")
-
-# Define company name:
-company_name = ""
-if company_name == "":
-    raise ValueError("Please define a company name in streamlit_app.py.")
-
-__login__obj = __login__(auth_token=st.secrets["courier_api_key"],
-                         company_name=company_name,
-                         width=200, height=250,
-                         logout_button_name='Logout', hide_menu_bool=False,
-                         hide_footer_bool=False,
-                         lottie_url='https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json')
-
-LOGGED_IN = __login__obj.build_login_ui()
-
-if LOGGED_IN == True:
-
-    if MODE == "dev":
-        user_email = st.secrets["admin_email"]
-    elif MODE == "prod":
-        user_email = st.experimental_user.get("email")
-    else:
-        raise ValueError("Unknown mode")
-
-    st.write(f"Hello {user_email}!")
-
-    st.header("EncrypedCookieManager:")
-
-    @st.cache_resource(experimental_allow_widgets=True)
-    def get_manager():
-        return stx.CookieManager()
-
-    cookie_manager = get_manager()
-
-    st.subheader("All Cookies:")
-    cookies = cookie_manager.get_all()
-    st.write(cookies)
+auth_state = st.session_state["authentication_status"]
+if auth_state is False or auth_state is None:
+    st.error('Username/password is incorrect')
+    try:
+        email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(
+            preauthorization=False)
+        if email_of_registered_user:
+            st.success('User registered successfully')
+            with open('config.yaml', 'w') as file:
+                yaml.dump(config, file, default_flow_style=False)
+    except Exception as e:
+        st.error(e)
+else:
+    # LOGGED IN CONTENT
+    pass
